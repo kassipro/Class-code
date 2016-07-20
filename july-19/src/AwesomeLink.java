0d323c85db2400c356b2c28fddc6e2790792fa02
Cole Frederick <csp.frederick@gmail.com>
2016-07-19 22:26:57 -0600

    basic list functions

---
+++ july-19/src/AwesomeLink.java
@@ -0,0 +1,41 @@
+public class AwesomeLink {
+    private int data;
+    private AwesomeLink next;
+
+    public AwesomeLink(int data, AwesomeLink next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public AwesomeLink(int data) {
+        this(data, null);
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public AwesomeLink getNext() {
+        return next;
+    }
+
+    public void setData(int newData) {
+        data = newData;
+    }
+
+    public void setNext(AwesomeLink newNext) {
+        next = newNext;
+    }
+
+    public static void main(String[] args) {
+        AwesomeLink a = new AwesomeLink(1);
+        System.out.println("AwesomeLink a holds: " + a.getData());
+        AwesomeLink b = new AwesomeLink(2, a);
+        System.out.println("AwesomeLink b holds: " + b.getData());
+
+        AwesomeLink head = null;
+        for(int i = 0; i < 10; i++) {
+            head = new AwesomeLink(i, head);
+        }
+    }
+}
